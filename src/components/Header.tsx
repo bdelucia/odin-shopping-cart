47cffa446db678412da6ff129d36b53108722777
Robert DeLucia Jr. <bdelucia@asu.edu>
2025-06-08 16:43:17 -0700

    fixed: right sidebar not disabling input

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,11 @@
 import '../index.css';
-import Sidebar from './LeftSidebar';
-import RightSidebar from './RightSidebar';
+import { LeftSidebar, RightSidebar } from './Sidebars';
 
 function Header() {
   return (
     <header className="navbar bg-primary text-primary-content fixed top-0 left-0 right-0 h-[7vh] z-[999]">
       <div className="navbar-start">
-        <Sidebar />
+        <LeftSidebar />
       </div>
       <div className="navbar-center">
         <a className="btn normal-case text-xl">The Bob Shop</a>
