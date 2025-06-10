876f73fcd92ed1a96b3033e6ce9d7af20eeb8347
Robert DeLucia Jr. <bdelucia@asu.edu>
2025-06-10 08:34:16 -0700

    fix: added unique key to container

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -36,7 +36,10 @@ function ThemeSwitcher() {
   return (
     <div className="join join-vertical theme-container">
       {themes.map((theme) => (
-        <button className="theme-button flex justify-center items-center gap-1 sm:gap-2 px-2 sm:px-4">
+        <button
+          key={theme}
+          className="theme-button flex justify-center items-center gap-1 sm:gap-2 px-2 sm:px-4"
+        >
           <div
             data-theme={theme}
             className="theme-preview bg-base-100 size-5 sm:size-7 grid shrink-0 grid-cols-2 gap-0.5 rounded-md p-0.5 sm:p-1 shadow-sm items-center"
@@ -47,7 +50,6 @@ function ThemeSwitcher() {
             <div className="bg-accent size-1.5 sm:size-2 rounded-full"></div>
           </div>
           <input
-            key={theme}
             type="radio"
             name="theme-buttons"
             className="btn btn-sm landscape-input sm:btn-md theme-controller join-item min-w-full h-auto py-1 sm:py-2"
