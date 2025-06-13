6bb0da4b88d55be8124a10a48b9df5ab819eae3d
Robert DeLucia Jr. <bdelucia@asu.edu>
2025-06-12 17:27:08 -0700

    feat: header shadowing, better cardskeleton color

--- src/components/cards/ShopCardSkeleton.tsx
+++ src/components/cards/ShopCardSkeleton.tsx
@@ -2,7 +2,7 @@ export function ShopCardSkeleton() {
   return (
     <div className="flex flex-col h-full bg-base-100 shadow-sm z-0 rounded-2xl border-3 border-base-content box-border">
       <figure className="overflow-hidden rounded-t-xl">
-        <div className="w-full h-48 bg-base-content animate-pulse p-4"></div>
+        <div className="w-full h-48 bg-white animate-pulse p-4"></div>
       </figure>
       <div className="card-body p-3 sm:p-4 lg:p-6 flex flex-col flex-grow">
         <div className="h-3.5 sm:h-4 lg:h-5 bg-base-300 rounded w-3/4 animate-pulse"></div>
