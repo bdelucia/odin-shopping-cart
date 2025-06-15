0ebbd21c13f6a6234cb0cfa7f302f5d1b995306b
Robert DeLucia Jr. <bdelucia@asu.edu>
2025-06-14 18:36:30 -0700

    feat: vercel analytics feature

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,9 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import routes from './routes';
+import { inject } from '@vercel/analytics';
 
+inject();
 const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById('root')!).render(
