e0fa390e6a0d110fac15055ec0119dbd50a35597
Robert DeLucia Jr. <bdelucia@asu.edu>
2025-06-14 18:43:04 -0700

    feat: vercel speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,10 @@ import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import routes from './routes';
 import { inject } from '@vercel/analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights';
 
 inject();
+injectSpeedInsights();
 const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById('root')!).render(
